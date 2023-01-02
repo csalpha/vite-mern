5576615a1d3599765830472d5aca95ee5b96b7dc
Carlos Serôdio <carlosserodio86@hotmail.com>
2023-01-02 16:25:26 +0000

    Install Tailwind CSS

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-1
+1. Install Tailwind CSS
+
+   1. npm install -D tailwindcss postcss autoprefixer
+   2. npx tailwindcss init -p
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -53,16 +53,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className='card'>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
